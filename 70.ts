a76bfc717406a92f45f69570ec74175b43c4bf62
muilyang12 <78548830+muilyang12@users.noreply.github.com>
2023-02-22 08:16:31 +0900

    LeetCode 70

--- 70.ts
+++ 70.ts
@@ -4,7 +4,7 @@
 
 // Dynamic Programming
 // time complexity: O(n) || space complexity: O(n)
-function climbStairs(n: number): number {
+function climbStairs1(n: number): number {
   let result = 0;
 
   const dp: number[] = [];
@@ -19,4 +19,21 @@ function climbStairs(n: number): number {
   result = dp[n];
 
   return result;
+};
+
+// Dynamic Programming
+// time complexity: O(n) || space complexity: O(2) = O(1)
+function climbStairs2(n: number): number {
+    let dpVar01 = 1;
+    let dpVar02 = 2;
+
+    for (let i = 3; i < n + 1; i++) {
+        const tempResult = dpVar01 + dpVar02;
+
+        dpVar01 = dpVar02;
+        dpVar02 = tempResult;
+    }
+
+    if (n < 3) return n;
+    else  return dpVar02;
 };
