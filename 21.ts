ba98ae6bab01b228e5845c2a5a1b9174ed49ba44
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2023-02-28 07:40:16 +0900

    LeetCode 21

---
+++ 21.ts
@@ -0,0 +1,54 @@
+// 21. Merge Two Sorted Lists
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+// Merge Sort
+// time complexity: O(m + n) || space complexity: O(1)
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  if (!list1 && !list2) return null;
+
+  const dummy: ListNode = new ListNode();
+  let resultPointer: ListNode = dummy;
+
+  let list1Pointer = list1;
+  let list2Pointer = list2;
+
+  while (list1Pointer && list2Pointer) {
+    if (list1Pointer.val <= list2Pointer.val) {
+      resultPointer.next = list1Pointer;
+      list1Pointer = list1Pointer.next;
+    } else {
+      resultPointer.next = list2Pointer;
+      list2Pointer = list2Pointer.next;
+    }
+
+    resultPointer = resultPointer.next;
+  }
+
+  while (list1Pointer) {
+    resultPointer.next = list1Pointer;
+    resultPointer = resultPointer.next;
+
+    list1Pointer = list1Pointer.next;
+  }
+
+  while (list2Pointer) {
+    resultPointer.next = list2Pointer;
+    resultPointer = resultPointer.next;
+
+    list2Pointer = list2Pointer.next;
+  }
+
+  return dummy.next;
+}
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
