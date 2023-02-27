3654c9700eadae28d5b411c47ca1022e599e6d74
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2023-02-28 08:16:29 +0900

    LeetCode 21 두 번째 방식 추가.

--- 21.ts
+++ 21.ts
@@ -3,7 +3,7 @@
 
 // Merge Sort
 // time complexity: O(m + n) || space complexity: O(1)
-function mergeTwoLists(
+function mergeTwoLists1(
   list1: ListNode | null,
   list2: ListNode | null
 ): ListNode | null {
@@ -36,6 +36,51 @@ function mergeTwoLists(
 
   while (list2Pointer) {
     resultPointer.next = list2Pointer;
+    resultPointer = resultPointer.next;
+
+    list2Pointer = list2Pointer.next;
+  }
+
+  return dummy.next;
+}
+
+// Merge Sort
+// time complexity: O(m + n) || space complexity: O(n + m)
+function mergeTwoLists2(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  if (!list1 && !list2) return null;
+
+  const dummy: ListNode = new ListNode();
+  let resultPointer: ListNode = dummy;
+
+  let list1Pointer = list1;
+  let list2Pointer = list2;
+
+  while (list1Pointer && list2Pointer) {
+    if (list1Pointer.val <= list2Pointer.val) {
+      resultPointer.next = new ListNode(list1Pointer.val);
+      resultPointer = resultPointer.next;
+
+      list1Pointer = list1Pointer.next;
+    } else {
+      resultPointer.next = new ListNode(list2Pointer.val);
+      resultPointer = resultPointer.next;
+
+      list2Pointer = list2Pointer.next;
+    }
+  }
+
+  while (list1Pointer) {
+    resultPointer.next = new ListNode(list1Pointer.val);
+    resultPointer = resultPointer.next;
+
+    list1Pointer = list1Pointer.next;
+  }
+
+  while (list2Pointer) {
+    resultPointer.next = new ListNode(list2Pointer.val);
     resultPointer = resultPointer.next;
 
     list2Pointer = list2Pointer.next;
