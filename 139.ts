8430ce88d012a5ad1b8d7c23c6d7ab22d647048c
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2023-03-02 22:23:56 +0900

    LeetCode 139

--- 139.ts
+++ 139.ts
@@ -3,7 +3,7 @@
 
 // Divide-and-conquer
 // time complexity: O(n^3) || space complexity: O(n)
-function wordBreak(s: string, wordDict: string[]): boolean {
+function wordBreak1(s: string, wordDict: string[]): boolean {
   let result = false;
 
   const cuttingPoint: number[] = [];
@@ -21,8 +21,28 @@ function wordBreak(s: string, wordDict: string[]): boolean {
     const nextString = s.slice(cuttingPoint[i] + 1);
     if (nextString === "") return true;
 
-    result = result || wordBreak(nextString, wordDict);
+    result = result || wordBreak1(nextString, wordDict);
   }
 
   return result;
+}
+
+// Dynamic Programming
+//
+function wordBreak2(s: string, wordDict: string[]): boolean {
+  const wordSet = new Set(wordDict);
+
+  const dp: boolean[] = Array(s.length + 1).fill(false);
+  dp[0] = true;
+
+  for (let i = 1; i <= s.length; i++) {
+    for (let j = 0; j < i; j++) {
+      const str = s.slice(j, i);
+      if (wordSet.has(str) && dp[j]) {
+        dp[i] = dp[j];
+      }
+    }
+  }
+
+  return dp[dp.length - 1];
 }
