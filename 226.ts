3ffce0bdca98506b81157e27bc4b191baa41e3b5
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2023-03-04 18:30:54 +0900

    LeetCode 226

--- 226.ts
+++ 226.ts
@@ -2,7 +2,7 @@
 // https://leetcode.com/problems/invert-binary-tree/
 
 // Divide-and-conquer
-// // time complexity: O(n) || space complexity: O(n) => Depth of stack
+// time complexity: O(n) || space complexity: O(n) => Depth of stack
 function invertTree1(root: TreeNode | null): TreeNode | null {
   if (root === null) return root;
   if (root.left === null && root.right === null) return root;
@@ -17,13 +17,37 @@ function invertTree1(root: TreeNode | null): TreeNode | null {
   return root;
 }
 
-class TreeNode {
-    val: number;
-    left: TreeNode | null;
-    right: TreeNode | null;
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-      this.val = val === undefined ? 0 : val;
-      this.left = left === undefined ? null : left;
-      this.right = right === undefined ? null : right;
-    }
+// Depth First Search (DFS) // Stack
+// time complexity: O(n) || space complexity: O(n)
+function invertTree2(root: TreeNode | null): TreeNode | null {
+  if (root === null) return root;
+
+  const stack: TreeNode[] = [];
+  stack.push(root);
+
+  while (stack.length > 0) {
+    const node = stack.pop();
+
+    if (!node) break;
+
+    const temp = node.left;
+    node.left = node.right;
+    node.right = temp;
+
+    if (node.left) stack.push(node.left);
+    if (node.right) stack.push(node.right);
   }
+
+  return root;
+}
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
