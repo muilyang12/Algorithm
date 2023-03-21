1d4ecb5706c18d39a6f814e2951f5b5f5d262c05
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2023-03-21 21:18:40 +0900

    LeetCode 139

--- 139.js
+++ 139.js
@@ -8,7 +8,37 @@
  */
 
 // Divide-and-conquer
-var wordBreak = function (s, wordDict) {};
+// time complexity: O(2^n) || space complexity: O(n)
+// How I can calculate the time complexity: https://stackoverflow.com/questions/31370674/time-complexity-of-the-word-break-recursive-solution
+var wordBreak = function (s, wordDict) {
+  const wordSet = new Set(wordDict);
+
+  const dfs = function (s) {
+    if (s.length === 0) {
+      return true;
+    }
+
+    let target;
+    let tempResult;
+    for (let i = 0; i < s.length; i++) {
+      target = s.slice(0, i + 1);
+
+      if (!wordSet.has(target)) {
+        continue;
+      }
+
+      const nextString = s.slice(i + 1);
+      tempResult = dfs(nextString);
+      if (tempResult) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
+  return dfs(s);
+};
 
 // Dynamic Programming
 // time complexity: O(w + n^2 * w) = O(n^2 * w) || space complexity: O(n)
