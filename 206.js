a75e3eb0884d3f6e21c86832eb6e76435b12d287
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2023-05-15 22:12:11 +0900

    LeetCode 206 시작.

---
+++ 206.js
@@ -0,0 +1,10 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {};
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
