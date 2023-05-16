a80aa3b2f94b0081b454cf57dcbf6a5eb11e0caa
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2023-05-16 20:12:06 +0900

    LeetCode 206 Stack 방식.

--- 206.js
+++ 206.js
@@ -2,7 +2,29 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function (head) {};
+
+// Stack
+// time complexity: O(2n) = O(n) || space complexity: O(n)
+var reverseList1 = function (head) {
+  const stack = [];
+
+  let headPointer = head;
+  while (!!headPointer) {
+    stack.push(headPointer.val);
+
+    headPointer = headPointer.next;
+  }
+
+  const result = new ListNode(-1);
+
+  let resultPointer = result;
+  for (let i = stack.length - 1; i >= 0; i--) {
+    resultPointer.next = new ListNode(stack[i]);
+    resultPointer = resultPointer.next;
+  }
+
+  return result.next;
+};
 
 function ListNode(val, next) {
   this.val = val === undefined ? 0 : val;
