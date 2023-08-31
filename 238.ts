531651c6364dda6631cc9518836f272b065393e5
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2023-09-01 07:36:46 +0900

    LeetCode 75 완성.

---
+++ 238.ts
@@ -0,0 +1,44 @@
+function productExceptSelf1(nums: number[]): number[] {
+  return nums.map((num, index) => {
+    let result = 1;
+
+    for (let j = 0; j < nums.length; j++) {
+      if (index !== j) {
+        result *= nums[j];
+      }
+    }
+
+    return result;
+  });
+}
+
+function productExceptSelf2(nums: number[]): number[] {
+  const memo: number[] = [];
+
+  return nums.map((num, index) => {
+    let result = 1;
+
+    if (index - 1 >= 0 && memo[index - 1]) {
+      result *= memo[index - 1];
+
+      for (let i = index + 1; i < nums.length; i++) {
+        result *= nums[i];
+      }
+    } else {
+      let valueToMemo = 1;
+      for (let i = 0; i <= index - 1; i++) {
+        valueToMemo *= nums[i];
+      }
+
+      memo[index - 1] = valueToMemo;
+
+      result *= memo[index - 1];
+
+      for (let i = index + 1; i < nums.length; i++) {
+        result *= nums[i];
+      }
+    }
+
+    return result;
+  });
+}
