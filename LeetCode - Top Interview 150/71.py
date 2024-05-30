4e6a438253d4b4f8c9c4a9665b6199860be495b7
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-05-30 11:11:14 +0900

    LeetCode 71 Python

--- LeetCode - Top Interview 150/71.py
+++ LeetCode - Top Interview 150/71.py
@@ -3,5 +3,57 @@
 
 
 class Solution:
-    def simplifyPath(self, path: str) -> str:
-        return ""
+    def simplifyPath1(self, path: str) -> str:
+        result = []
+
+        splited_path = path.split("/")
+
+        for i in range(len(splited_path)):
+            part = splited_path[i]
+            if part == "..":
+                if len(result) > 1:
+                    result.pop()
+                else:
+                    continue
+
+            elif part == ".":
+                continue
+
+            elif part == "" and i != 0:
+                continue
+
+            else:
+                result.append(part)
+
+        return "/".join(result) if len(result) > 1 else "/"
+
+    def simplifyPath2(self, path: str) -> str:
+        result = []
+
+        splited_path = path.split("/")
+
+        for part in splited_path:
+            if part == "..":
+                if result:
+                    result.pop()
+                else:
+                    continue
+
+            elif part == "." or part == "":
+                continue
+
+            else:
+                result.append(part)
+
+        return "/" + "/".join(result)
+
+
+solution = Solution()
+
+print(solution.simplifyPath2(path="/home/"))
+print(solution.simplifyPath2(path="/home//foo/"))
+print(solution.simplifyPath2(path="/home/user/Documents/../Pictures"))
+print(solution.simplifyPath2(path="/../"))
+print(solution.simplifyPath2(path="/.../a/../b/c/../d/./"))
+print(solution.simplifyPath2(path="/a/../../b/../c//.//"))
+print(solution.simplifyPath2(path="/../"))
