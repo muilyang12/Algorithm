9b652bf45d0d701386c16f15afd9967bd28a06ee
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-03 07:46:21 +0900

    LeetCode 57 Python 다른 풀이 추가.

--- LeetCode - Top Interview 150/57.py
+++ LeetCode - Top Interview 150/57.py
@@ -6,7 +6,7 @@ from typing import List
 
 
 class Solution:
-    def insert(
+    def insert1(
         self, intervals: List[List[int]], newInterval: List[int]
     ) -> List[List[int]]:
         new_start, _ = newInterval
@@ -49,13 +49,41 @@ class Solution:
 
         return result
 
+    def insert2(
+        self, intervals: List[List[int]], newInterval: List[int]
+    ) -> List[List[int]]:
+        added_intervals = intervals[:]
+        new_start, _ = newInterval
+
+        for i in range(len(added_intervals)):
+            start, _ = added_intervals[i]
+
+            if new_start < start:
+                added_intervals.insert(i, newInterval)
+                break
+
+        if len(added_intervals) == len(intervals):
+            added_intervals.append(newInterval)
+
+        result = [added_intervals[0]]
+
+        for start, end in added_intervals[1:]:
+            _, last_end = result[-1]
+
+            if start <= last_end:
+                result[-1][1] = max(last_end, end)
+            else:
+                result.append([start, end])
+
+        return result
+
 
 solution = Solution()
 
-print(solution.insert(intervals=[[1, 3], [6, 9]], newInterval=[2, 5]))
+print(solution.insert2(intervals=[[1, 3], [6, 9]], newInterval=[2, 5]))
 print(
-    solution.insert(
+    solution.insert2(
         intervals=[[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], newInterval=[4, 8]
     )
 )
-print(solution.insert(intervals=[], newInterval=[5, 7]))
+print(solution.insert2(intervals=[], newInterval=[5, 7]))
