67480fec08c0faa546a777deb3b9421b0dd0e956
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-05 20:06:48 +0900

    LeetCode 92 Python 다른 풀이 추가.

--- LeetCode - Top Interview 150/92.py
+++ LeetCode - Top Interview 150/92.py
@@ -12,7 +12,7 @@ class ListNode:
 
 
 class Solution:
-    def reverseBetween(
+    def reverseBetween1(
         self, head: Optional[ListNode], left: int, right: int
     ) -> Optional[ListNode]:
         dummy = ListNode(0, head)
@@ -39,8 +39,33 @@ class Solution:
 
         return dummy.next
 
+    def reverseBetween2(
+        self, head: Optional[ListNode], left: int, right: int
+    ) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+
+        before_left_node = dummy
+        for _ in range(left - 1):
+            before_left_node = before_left_node.next
+
+        after_right_node = before_left_node.next
+        for _ in range(right - left + 1):
+            after_right_node = after_right_node.next
+
+        current_node, next_node = before_left_node.next, after_right_node
+        for _ in range(right - left + 1):
+            temp_next_node = current_node.next
+            current_node.next = next_node
+
+            next_node = current_node
+            current_node = temp_next_node
+
+        before_left_node.next = next_node
+
+        return dummy.next
+
 
 solution = Solution()
 
-print(solution.reverseBetween(head=[1, 2, 3, 4, 5], left=2, right=4))
-print(solution.reverseBetween(head=[5], left=1, right=1))
+print(solution.reverseBetween1(head=[1, 2, 3, 4, 5], left=2, right=4))
+print(solution.reverseBetween1(head=[5], left=1, right=1))
