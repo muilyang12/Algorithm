c4a7d20397cc5b54ae46cdacc5ae131becef810b
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-08 13:13:57 +0900

    LeetCode 15 Python 다른 풀이 추가.

--- LeetCode - Top Interview 150/15.py
+++ LeetCode - Top Interview 150/15.py
@@ -9,7 +9,7 @@ class Solution:
     # time complexity: O(n^3)
     # Checking membership in a list takes O(n) ('in' operator)
     # Sicing operation takes O(n)
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum1(self, nums: List[int]) -> List[List[int]]:
         result = set()
 
         length = len(nums)
@@ -25,3 +25,57 @@ class Solution:
                 result.add(temp_result)
 
         return list(result)
+
+    # time complexity: O(n^2)
+    def threeSum2(self, nums: List[int]) -> List[List[int]]:
+        result = set()
+
+        length = len(nums)
+
+        for i in range(length - 2):
+            elems_between_i_j = set()
+
+            for j in range(i + 1, length):
+                complement = 0 - (nums[i] + nums[j])
+
+                if complement in elems_between_i_j:
+                    temp_result = tuple(sorted([nums[i], nums[j], complement]))
+                    result.add(temp_result)
+
+                elems_between_i_j.add(nums[j])
+
+        return list(result)
+
+    # time complexity: O(n^2)
+    def threeSum3(self, nums: List[int]) -> List[List[int]]:
+        result = set()
+
+        sorted_nums = sorted(nums)
+
+        length = len(nums)
+        for i in range(length - 2):
+            left = i + 1
+            right = length - 1
+
+            while left < right:
+                if sorted_nums[i] + sorted_nums[left] + sorted_nums[right] > 0:
+                    right -= 1
+                elif sorted_nums[i] + sorted_nums[left] + sorted_nums[right] < 0:
+                    left += 1
+                else:
+                    temp_result = tuple(
+                        sorted([sorted_nums[i], sorted_nums[left], sorted_nums[right]])
+                    )
+                    result.add(temp_result)
+
+                    left += 1
+                    right -= 1
+
+        return list(result)
+
+
+solution = Solution()
+
+print(solution.threeSum2(nums=[-1, 0, 1, 2, -1, -4]))
+print(solution.threeSum2(nums=[0, 1, 1]))
+print(solution.threeSum2(nums=[0, 0, 0]))
