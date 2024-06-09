a4e2acda3968b2fa398bc1e9c55d308f23a36042
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-09 12:18:42 +0900

    LeetCode 11 Python 잘못된 풀이 추가.

---
+++ LeetCode - Top Interview 150/11.py
@@ -0,0 +1,41 @@
+# 11. Container With Most Water
+# https://leetcode.com/problems/container-with-most-water/
+
+
+from typing import List
+
+
+class Solution:
+    def calculate_volume(self, height, left, right):
+        if not left < right:
+            return 0
+
+        current_volume = (right - left) * min(height[left], height[right])
+
+        return current_volume
+
+    # Idea was nice. But the answer is wrong. :(
+    def maxArea1(self, height: List[int]) -> int:
+        result = 0
+
+        left = 0
+        right = len(height) - 1
+
+        while left < right:
+            current_volume1 = self.calculate_volume(height, left, right)
+            current_volume2 = self.calculate_volume(height, left + 1, right)
+            current_volume3 = self.calculate_volume(height, left, right - 1)
+
+            result = max(result, current_volume1, current_volume2, current_volume3)
+
+            left += 1
+            right -= 1
+
+        return result
+
+
+solution = Solution()
+
+print(solution.maxArea1(height=[1, 8, 6, 2, 5, 4, 8, 3, 7]))
+print(solution.maxArea1(height=[1, 1]))
+print(solution.maxArea1(height=[2, 3, 10, 5, 7, 8, 9]))
