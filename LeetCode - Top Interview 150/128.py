78f18d4f4827ad6cd2eb0ac5b3cbd805769b37f4
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-14 22:43:15 +0900

    LeetCode 128 Python 시작.

---
+++ LeetCode - Top Interview 150/128.py
@@ -0,0 +1,11 @@
+# 128. Longest Consecutive Sequence
+# https://leetcode.com/problems/longest-consecutive-sequence/
+
+
+from typing import List
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+
+        return 0
