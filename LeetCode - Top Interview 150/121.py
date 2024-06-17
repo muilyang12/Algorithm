5660ffdc29488bbdf9e04511dfd16528d5588590
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-17 21:08:27 +0900

    LeetCode 121 Python 시작.

---
+++ LeetCode - Top Interview 150/121.py
@@ -0,0 +1,9 @@
+# 121. Best Time to Buy and Sell Stock
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
