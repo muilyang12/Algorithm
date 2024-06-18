d5c86f984b35f7d8836fb4c57f06585728e9b792
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-18 10:19:52 +0900

    LeetCode 121, 122 Python.

--- LeetCode - Top Interview 150/121.py
+++ LeetCode - Top Interview 150/121.py
@@ -4,6 +4,27 @@
 
 from typing import List
 
+
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        
+    # time complexity: O(n ^ 2)
+    def maxProfit1(self, prices: List[int]) -> int:
+        max_profit = 0
+
+        for i in range(len(prices) - 1):
+            buying_price = prices[i]
+            selling_price = max(prices[i + 1 :])
+
+            max_profit = max(max_profit, selling_price - buying_price)
+
+        return max_profit
+
+    # time complexity: O(n)
+    def maxProfit2(self, prices: List[int]) -> int:
+        buying_price = prices[0]
+        max_profit = 0
+
+        for price in prices:
+            buying_price = min(buying_price, price)
+            max_profit = max(max_profit, price - buying_price)
+
+        return max_profit
---
+++ LeetCode - Top Interview 150/122.py
@@ -0,0 +1,18 @@
+# 122. Best Time to Buy and Sell Stock II
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profits = [0]
+
+        for i in range(1, len(prices)):
+            if prices[i] <= prices[i - 1]:
+                continue
+
+            max_profits.append(max_profits[-1] + (prices[i] - prices[i - 1]))
+
+        return max_profits[-1]
