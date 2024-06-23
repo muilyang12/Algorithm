bd07cac8909fc81acc27a506bf0ee63a21cd26bf
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-23 20:36:47 +0900

    LeetCode 146 Python 다른 풀이 추가.

--- LeetCode - Top Interview 150/146.py
+++ LeetCode - Top Interview 150/146.py
@@ -28,3 +28,67 @@ class LRUCache1:
             target_key = self.inserted_sequence.pop(0)
             del self.cache[target_key]
 
+
+class LRUCache2:
+    class Node:
+        def __init__(self, key, value):
+            self.key = key
+            self.value = value
+            self.prev = None
+            self.next = None
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+
+        self.head = self.Node("head", -1)
+        self.tail = self.Node("tail", -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+        self.cache = {}
+
+    def get(self, key: int) -> int:
+        if not key in self.cache:
+            return -1
+
+        target_node = self.cache[key]
+        target_key = target_node.key
+        target_value = target_node.value
+
+        self.remove_node(target_key)
+        self.append_node(target_key, target_value)
+
+        return target_value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove_node(key)
+
+        self.append_node(key, value)
+
+        if len(self.cache) > self.capacity:
+            first_node = self.head.next
+            self.remove_node(first_node.key)
+
+    def append_node(self, key, value):
+        new_node = self.Node(key, value)
+
+        last_node = self.tail.prev
+
+        last_node.next = new_node
+        new_node.next = self.tail
+        new_node.prev = last_node
+        self.tail.prev = new_node
+
+        self.cache[key] = new_node
+
+    def remove_node(self, key):
+        target_node = self.cache[key]
+
+        prev_node = target_node.prev
+        next_node = target_node.next
+
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+        del self.cache[key]
