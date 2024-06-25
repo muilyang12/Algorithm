f6882ed7a78ac966ab610ef9dd3cba6ee6b3dc4a
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-25 11:08:09 +0900

    LeetCode 105 Python 다른 풀이 추가.

--- LeetCode - Top Interview 150/105.py
+++ LeetCode - Top Interview 150/105.py
@@ -13,7 +13,10 @@ class TreeNode:
 
 
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+    # Divide and conquer
+    # time complexity: O(n ^ 2) (At the worst)
+    # Sicing operation takes O(n)
+    def buildTree1(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if not preorder or not inorder:
             return None
 
@@ -29,7 +32,39 @@ class Solution:
 
         root = TreeNode(root_value)
 
-        root.left = self.buildTree(preorder_left, inorder_left)
-        root.right = self.buildTree(preorder_right, inorder_right)
+        root.left = self.buildTree1(preorder_left, inorder_left)
+        root.right = self.buildTree1(preorder_right, inorder_right)
 
         return root
+
+    # Divide and conquer
+    # time complexity: O(n) (At the worst)
+    def buildTree2(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+
+        inorder_value_index_map = {value: index for index, value in enumerate(inorder)}
+
+        def build(pre_start, pre_end, in_start, in_end):
+            if pre_start > pre_end:
+                return None
+
+            current_value = preorder[pre_start]
+            node = TreeNode(current_value)
+
+            inorder_index = inorder_value_index_map[current_value]
+            inorder_left_count = inorder_index - in_start
+
+            node.left = build(
+                pre_start + 1,
+                pre_start + inorder_left_count,
+                in_start,
+                inorder_index - 1,
+            )
+            node.right = build(
+                pre_start + inorder_left_count + 1, pre_end, inorder_index + 1, in_end
+            )
+
+            return node
+
+        return build(0, len(preorder) - 1, 0, len(inorder) - 1)
