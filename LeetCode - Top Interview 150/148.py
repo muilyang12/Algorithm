3e63a618f2a5faad563db0f393d2179108772e2d
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-08-17 04:27:26 -0500

    LeetCode 148 Python

--- LeetCode - Top Interview 150/148.py
+++ LeetCode - Top Interview 150/148.py
@@ -16,6 +16,51 @@ class Solution:
         if not head:
             return head
 
+        if not head.next:
+            return head
+
+        left = head
+        node_before_right = self.get_mid(head)
+        right = node_before_right.next
+        node_before_right.next = None
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+
+        dummy = ListNode(-1)
+        current = dummy
+
+        while left and right:
+            if left.val < right.val:
+                current.next = left
+
+                left = left.next
+                current = current.next
+
+            else:
+                current.next = right
+
+                right = right.next
+                current = current.next
+
+        if left:
+            current.next = left
+
+        if right:
+            current.next = right
+
+        return dummy.next
+
+    # This approach looks awesome!
+    def get_mid(self, head: ListNode) -> ListNode:
+        slow, fast = head, head
+
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
     # Basic Merge Sort
     # time complexity: O(nlogn)
     def basic_merge_sort(self, arr: List[int]):
