e52787155dbcbfe12421919ae225a1aa3ff6b3cc
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-09-01 06:57:27 -0500

    LeetCode 49 Python valid key에 대한 설명 추가.

---
+++ LeetCode - Algorithm/Hash/49.py
@@ -0,0 +1,39 @@
+# 49. Group Anagrams
+# https://leetcode.com/problems/group-anagrams/
+
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash = defaultdict(list)
+
+
+"""
+Requirements for Keys in dict and defaultdict
+- Immutable
+- Hashable
+
+Examples of Valid Keys:
+- Integers: 1, 2, 3
+- Floats: 3.14, 2.71
+- Strings: "apple", "banana", "cherry"
+- Tuples: ("a", 1), (3.14, "pi")
+- Booleans: True, False
+- NoneType: None
+
+Examples of Invalid Keys:
+- Lists: [1, 2, 3]
+- Dictionaries: {"key": "value"}
+- Sets: {1, 2, 3}
+
+-> The code below doesn't work because list type is not a valid key in the dictionary.
+
+   hash = default(list)
+   data = [1, 2, 3]
+
+   hash[data].append("123")
+
+
+"""
