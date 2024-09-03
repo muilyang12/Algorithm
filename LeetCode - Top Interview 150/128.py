325f59e5d26df09bc0a7d09da3463bea4a3114e5
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-09-03 06:07:17 -0500

    LeetCode 128 Python

--- LeetCode - Top Interview 150/128.py
+++ LeetCode - Top Interview 150/128.py
@@ -7,5 +7,30 @@ from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        nums_set = set(nums)
 
-        return 0
+        result = 0
+
+        target = None
+
+        for num in nums:
+            if num - 1 in nums_set:
+                continue
+
+            current_result = 1
+
+            target = num
+            while target + 1 in nums_set:
+                current_result += 1
+
+                target += 1
+
+            result = max(result, current_result)
+
+        return result
+
+
+solution = Solution()
+
+print(solution.longestConsecutive(nums=[100, 4, 200, 1, 3, 2]))
+print(solution.longestConsecutive(nums=[0, 3, 7, 2, 5, 8, 4, 6, 0, 1]))
