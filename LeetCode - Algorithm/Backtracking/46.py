b3f867507c8bbf7469611d84f20007610791f31d
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-09-06 06:26:59 -0500

    LeetCode 46 Python. 새로운 풀이법 및 설명 추가.

--- LeetCode - Algorithm/Backtracking/46.py
+++ LeetCode - Algorithm/Backtracking/46.py
@@ -26,3 +26,43 @@ class Solution:
                 self.makePermutationList(nums, used, result)
 
             used.remove(num)
+
+    def permute2(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        self.makePermutationList2(nums, [], [False] * len(nums), result)
+
+        return result
+
+    def makePermutationList2(
+        self, nums: int, path: List[int], visited: List[bool], result: List[int]
+    ):
+        for index, num in enumerate(nums):
+            if visited[index]:
+                continue
+
+            path.append(num)
+            visited[index] = True
+
+            if len(path) == len(nums):
+                result.append(list(path))
+            else:
+                self.makePermutationList2(nums, path, visited, result)
+
+            path.remove(num)
+            visited[index] = False
+
+
+"""
+In my previous solution, I checked at each step of the loop whether a new number already existed in the current 'path'. 
+Realizing this was inefficient, I decided to use a set for the 'path' to speed up the checking process. Then, I converted 
+the 'path' back to a list just before adding it to the result. However it did not work.
+
+The new method introduces an additional argument called 'visited,' which is initialized as a list of False values with 
+a length equal to len(nums). Each time a new value is added to the path, the corresponding index in the ' visited' array
+is set to True. This way, we no longer need to check if a specific value exists in the 'path'. Instead, we only need to
+verify whether the value in the 'visited' array is False.
+
+Realizing that checking for the existence of a number in every loop was inefficient was a great insight. The approach using
+a set was a good attempt, but I need to remember the 'visited' array method going forward.
+"""
