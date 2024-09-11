62329cf9ac494d094f3ed9edc2de5535ee28995f
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-09-11 06:10:05 -0500

    LeetCode 46 Python 다른 풀이 및 설명 추가.

--- LeetCode - Algorithm/Heap/215.py
+++ LeetCode - Algorithm/Heap/215.py
@@ -7,7 +7,8 @@ import heapq
 
 
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    # time complexity: O(n log n + k log n) = O(n log n)
+    def findKthLargest1(self, nums: List[int], k: int) -> int:
         minHeap = []
 
         for num in nums:
@@ -17,3 +18,20 @@ class Solution:
                 heapq.heappop(minHeap)
 
         return heapq.heappop(minHeap)
+
+    # time complexity: O(k + 2 (n - k) log k) = O(n log k)
+    def findKthLargest2(self, nums: List[int], k: int) -> int:
+        minHeap = nums[:k]
+        heapq.heapify(minHeap)
+
+        for num in nums[k:]:
+            heapq.heappush(minHeap, num)
+            heapq.heappop(minHeap)
+
+        return minHeap[0]
+
+
+"""
+For returning the final result, I don't need to use heapq.pop(minHeap); 
+instead, I can simply use minHeap[0].
+"""
