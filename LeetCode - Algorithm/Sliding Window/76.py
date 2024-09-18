a788d2f88eb89fe21e526e78b3f53360b7eb592f
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-09-17 21:25:22 -0500

    LeetCode 76 Python 다른 풀이 추가.

--- LeetCode - Algorithm/Sliding Window/76.py
+++ LeetCode - Algorithm/Sliding Window/76.py
@@ -61,6 +61,66 @@ class Solution:
 
         return True
 
+    def minWindow2(self, s: str, t: str) -> str:
+        left = 0
+        right = 0
+
+        targetAlphabetCounts = defaultdict(int)
+        for char in t:
+            targetAlphabetCounts[char] += 1
+
+        isRightMoving = True
+
+        currentAlphabetCounts = defaultdict(int)
+        currentAlphabetCounts[s[left]] += 1
+
+        result = ""
+
+        while left <= right < len(s):
+            if (
+                not self.doesCurrentIncludeAllAlphabets(
+                    currentAlphabetCounts, targetAlphabetCounts
+                )
+                and isRightMoving
+            ):
+                right += 1
+                if right < len(s):
+                    currentAlphabetCounts[s[right]] += 1
+
+            elif (
+                self.doesCurrentIncludeAllAlphabets(
+                    currentAlphabetCounts, targetAlphabetCounts
+                )
+                and isRightMoving
+            ):
+                if not result or (result and right - left + 1 < len(result)):
+                    result = s[left : right + 1]
+
+                isRightMoving = not isRightMoving
+
+                currentAlphabetCounts[s[left]] -= 1
+                left += 1
+
+            elif not isRightMoving and not s[left] in t:
+                currentAlphabetCounts[s[left]] -= 1
+                left += 1
+
+            elif not isRightMoving and s[left] in t:
+                isRightMoving = not isRightMoving
+
+        return result
+
+    def doesCurrentIncludeAllAlphabets(
+        self,
+        currentAlphabetCounts: Dict[str, int],
+        targetAlphabetCounts: Dict[str, int],
+    ):
+        for alphabet, count in targetAlphabetCounts.items():
+            if currentAlphabetCounts[alphabet] < count:
+                return False
+
+        return True
+
 
 """
 The reason why my code exceeded the time limit is due to the inefficiency in the isAllAlphabetsIncluded
