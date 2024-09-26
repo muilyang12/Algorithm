0d96b20fee8c2cc2513ccf570609bc3ee40e924d
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-09-26 09:54:38 -0500

    LeetCode 19 JS

---
+++ LeetCode - Algorithm/Linked List/19.js
@@ -0,0 +1,34 @@
+// 19. Remove Nth Node From End of List
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+  let nodeBeforeSlow = null;
+  let slow = head;
+  let fast = head;
+
+  for (let i = 0; i < n; i++) {
+    fast = fast.next;
+  }
+
+  while (fast) {
+    fast = fast.next;
+    nodeBeforeSlow = slow;
+    slow = slow.next;
+  }
+
+  if (!nodeBeforeSlow) return slow.next;
+
+  nodeBeforeSlow.next = slow.next;
+
+  return head;
+};
