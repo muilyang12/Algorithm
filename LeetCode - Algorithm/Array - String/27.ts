3ce2fc26d9d4359177d8b364eb80f7904464c8df
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-10-02 10:15:58 -0500

    LeetCode 27 TS. Add some note.

--- LeetCode - Algorithm/Array - String/27.ts
+++ LeetCode - Algorithm/Array - String/27.ts
@@ -1,6 +1,8 @@
 // 27. Remove Element
 // https://leetcode.com/problems/remove-element/
 
+// time complexity: O(n)
+// space complexity: O(1)
 function removeElement(nums: number[], val: number): number {
   let result = 0;
 
@@ -28,7 +30,14 @@ function removeElement(nums: number[], val: number): number {
 }
 
 `
+This logic is also quite sound. Although maintaining the sequence wasn't necessary, I personally didn't 
+want to disrupt it, so I chose this approach. I used 'current' to iterate through the array, 'destination' 
+to keep track of where to place a new number that wasn't equal to 'val,' and 'result' to count the numbers 
+that differed from 'val.'
+`;
+
+`
 nums = [0,1,2,2,3,0,4,2], val = 2
-                ^
-des = 2
+        ^
+des = -1
 `;
