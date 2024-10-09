c372d8cc8e6b00c19f09230dd150d7e91a17f78f
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-10-08 21:25:17 -0500

    LeetCode 129 TS

---
+++ LeetCode - Algorithm/Binary Tree/129.ts
@@ -0,0 +1,34 @@
+// 129. Sum Root to Leaf Numbers
+// https://leetcode.com/problems/sum-root-to-leaf-numbers/
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function sumNumbers(root: TreeNode | null): number {
+  let result = 0;
+
+  function dfs(node: TreeNode, string: string = "") {
+    const newString = string + String(node.val);
+
+    if (!node.left && !node.right) {
+      result += Number(newString);
+
+      return;
+    }
+
+    if (node.left) dfs(node.left, newString);
+    if (node.right) dfs(node.right, newString);
+  }
+
+  root && dfs(root);
+
+  return result;
+}
