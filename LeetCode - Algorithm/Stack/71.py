2b97d0d4f84f23a0dd211d77f4bceab1770e1093
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-10-10 19:50:50 -0500

    LeetCode 71 Python

---
+++ LeetCode - Algorithm/Stack/71.py
@@ -0,0 +1,30 @@
+# 71. Simplify Path
+# https://leetcode.com/problems/simplify-path/
+
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        resultPathes = []
+
+        originalPathes = path.split("/")
+
+        for pathElem in originalPathes:
+            if pathElem == "":
+                continue
+            elif pathElem == ".":
+                continue
+            elif pathElem == "..":
+                if resultPathes:
+                    resultPathes.pop()
+            else:
+                resultPathes.append(pathElem)
+
+        result = "/"
+
+        for index, pathElem in enumerate(resultPathes):
+            result += pathElem
+
+            if index < len(resultPathes) - 1:
+                result += "/"
+
+        return result
