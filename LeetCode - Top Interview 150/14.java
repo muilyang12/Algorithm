d6e491e98aa9ec54e74f662cc626734f506c8e93
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-10-11 22:53:24 -0500

    LeetCode 274 Java

---
+++ LeetCode - Top Interview 150/14.java
@@ -0,0 +1,26 @@
+// 14. Longest Common Prefix
+// https://leetcode.com/problems/longest-common-prefix/
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+
+        String commonPrefix = strs[0];
+
+        int current = 1;
+
+        while (current < strs.length) {
+            String str = strs[current];
+
+            while (!str.startsWith(commonPrefix)) {
+                commonPrefix = commonPrefix.substring(0, commonPrefix.length() - 1);
+            }
+
+            current += 1;
+        }
+
+        return commonPrefix;
+    }
+}
