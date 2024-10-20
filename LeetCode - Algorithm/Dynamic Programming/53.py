d33cfd63b33c59b05d30b2ba8bc58f932081731e
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-10-19 22:03:41 -0500

    LeetCode 53 Python.

---
+++ LeetCode - Algorithm/Dynamic Programming/53.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        memo = [0] * len(nums)
+        memo[0] = nums[0]
+
+        for i in range(1, len(nums)):
+            memo[i] = max(memo[i - 1] + nums[i], nums[i])
+
+        return max(memo)
+
+
+"""
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+                            ^
+memo = [0,0,1,0,4,3,5,6,1,5]
+                          ^
+"""
