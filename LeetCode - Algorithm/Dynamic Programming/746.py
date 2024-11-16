6069cbdeef1e7924c8f02df4b71653d517684a1b
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-11-16 15:48:43 -0600

    LeetCode 746 Python.

--- LeetCode - Algorithm/Dynamic Programming/746.py
+++ LeetCode - Algorithm/Dynamic Programming/746.py
@@ -6,9 +6,18 @@ from typing import List
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        return 0
+        memo = [0] * len(cost)
+        memo[0] = cost[0]
+        memo[1] = cost[1]
+
+        for i in range(2, len(cost)):
+            memo[i] = min(memo[i - 2] + cost[i], memo[i - 1] + cost[i])
+
+        return min(memo[-1], memo[-2])
 
 
 """
 cost = [1,100,1,1,1,100,1,1,100,1]
+
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 """
