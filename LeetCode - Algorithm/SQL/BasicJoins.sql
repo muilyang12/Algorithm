28c09f944fc9bae7f0ec788da2298b1294e993c3
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-12-05 17:18:21 -0600

    Leetcode SQL 50 BasicJoins type. 2

--- LeetCode - Algorithm/SQL/BasicJoins.sql
+++ LeetCode - Algorithm/SQL/BasicJoins.sql
@@ -17,6 +17,24 @@ JOIN Weather AS W2
 ON W1.recordDate = W2.recordDate + INTERVAL '1 day'
 WHERE W1.temperature > W2.temperature;
 
+-- 1661. Average Time of Process per Machine
+-- https://leetcode.com/problems/average-time-of-process-per-machine/
+
+SELECT A1.machine_id, ROUND(CAST(AVG(A2.timestamp - A1.timestamp) AS NUMERIC), 3) AS processing_time
+FROM Activity as A1
+JOIN Activity as A2
+ON A1.machine_id = A2.machine_id AND A1.process_id = A2.process_id AND A1.activity_type = 'start' AND A2.activity_type = 'end'
+GROUP BY A1.machine_id;
+
+-- 577. Employee Bonus
+-- https://leetcode.com/problems/employee-bonus/
+
+SELECT E.name, B.bonus
+FROM Employee AS E
+LEFT OUTER JOIN Bonus AS B
+ON E.empId = B.empId
+WHERE B.bonus < 1000 OR B.bonus IS NULL;
+
 -- 1280. Students and Examinations
 -- https://leetcode.com/problems/students-and-examinations/
 
