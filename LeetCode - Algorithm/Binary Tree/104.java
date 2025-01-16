6214710c53b766abd98e778300648094c1bcaf75
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2025-01-16 10:38:54 -0600

    LeetCode 104 Java.

---
+++ LeetCode - Algorithm/Binary Tree/104.java
@@ -0,0 +1,55 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int result = 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int queueSize = queue.size();
+
+            for (int i = 0; i < queueSize; i++) {
+                TreeNode node = queue.poll();
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            result += 1;
+        }
+
+        return result;
+    }
+}
