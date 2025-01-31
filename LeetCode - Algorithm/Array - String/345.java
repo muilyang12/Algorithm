45a4ad57f8440ef40e3c5771fa7b976d9fdaa397
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2025-01-30 23:47:55 -0600

    LeetCode 345 Java.

---
+++ LeetCode - Algorithm/Array - String/345.java
@@ -0,0 +1,33 @@
+// 345. Reverse Vowels of a String
+// https://leetcode.com/problems/reverse-vowels-of-a-string/
+
+import java.util.Set;
+import java.util.Stack;
+
+class Solution {
+    public String reverseVowels(String s) {
+        Stack<Character> usedVowels = new Stack<>();
+
+        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+
+        for (int i = 0; i < s.length(); i++) {
+            if (vowels.contains(s.charAt(i))) {
+                usedVowels.push(s.charAt(i));
+            }
+        }
+
+        String result = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (vowels.contains(s.charAt(i))) {
+                result += usedVowels.pop();
+            } else {
+                result += s.charAt(i);
+            }
+        }
+
+        System.out.println(result);
+
+        return result;
+    }
+}
