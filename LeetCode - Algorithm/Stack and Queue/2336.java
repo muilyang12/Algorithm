743aa7593239e1a2df0b81861bc8140753db937e
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2025-02-08 23:15:48 -0600

    LeetCode 2336 Java.

---
+++ LeetCode - Algorithm/Stack and Queue/2336.java
@@ -0,0 +1,43 @@
+// 2336. Smallest Number in Infinite Set
+// https://leetcode.com/problems/smallest-number-in-infinite-set/
+
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.HashSet;
+
+class SmallestInfiniteSet {
+    PriorityQueue<Integer> minHeap;
+    int numberWhenEmpty;
+    Set<Integer> set;
+
+    public SmallestInfiniteSet() {
+        this.minHeap = new PriorityQueue<>();
+        this.numberWhenEmpty = 1;
+        this.set = new HashSet<>();
+    }
+
+    public int popSmallest() {
+        int result;
+
+        if (this.minHeap.size() == 0) {
+            result = numberWhenEmpty;
+            numberWhenEmpty++;
+        } else {
+            result = this.minHeap.poll();
+            this.set.remove(result);
+        }
+
+        return result;
+    }
+
+    public void addBack(int num) {
+        if (num >= this.numberWhenEmpty)
+            return;
+
+        if (this.set.contains(num))
+            return;
+
+        this.set.add(num);
+        this.minHeap.add(num);
+    }
+}
