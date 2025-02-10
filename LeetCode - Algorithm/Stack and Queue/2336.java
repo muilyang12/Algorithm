a96cd6f65d6d1bf3eeed4d0b2c51f8404b9c3840
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2025-02-09 18:04:31 -0600

    LeetCode 2336 Java. Add some explanation about Java grammar.

--- LeetCode - Algorithm/Stack and Queue/2336.java
+++ LeetCode - Algorithm/Stack and Queue/2336.java
@@ -41,3 +41,29 @@ class SmallestInfiniteSet {
         this.minHeap.offer(num);
     }
 }
+
+/*
+ * In order to use Min Heap, we can use PriorityQueue in Java.
+ * 
+ * PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+ * 
+ * 1. To add an element -> offer() / add()
+ * -> add() throws an exception if the queue is full
+ * 2. To retrieve and remove the minimum element -> poll() / remove()
+ * -> remove() throws an exception if the queue is empty.
+ * 3. To retrieve the minimum element without removing it -> peek() / element()
+ * -> element() throws an exception if the queue is empty.
+ */
+
+/*
+ * In Java,
+ * 
+ * - "int" is a primitive type, which is a basic data type in Java designed for
+ * efficiency.
+ * - "Integer" is a Wrapper Class, which is an object that wraps the primitive
+ * int and provides additional functionality.
+ * 
+ * Generics (like Stack<T>) do not support primitive types such as int and can
+ * only work with objects. So, we should use the wrapper class "Integer" not
+ * "int".
+ */
