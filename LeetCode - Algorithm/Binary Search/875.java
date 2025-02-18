283d4782fe40d83c31d58c06e26344627b350600
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2025-02-18 13:57:30 -0600

    LeetCode 875 Java. Add explanation about typecasting to get floating-point result from division.

--- LeetCode - Algorithm/Binary Search/875.java
+++ LeetCode - Algorithm/Binary Search/875.java
@@ -30,3 +30,15 @@ class Solution {
         return left;
     }
 }
+
+/*
+ * In Java, when performing int / int division, the decimal part is always
+ * truncated (discarded)
+ * 
+ * System.out.println(7 / 3); // -> Output: 2
+ * 
+ * So, if I want a floating-point result after division, you need to convert at
+ * least one operand to double.
+ * 
+ * System.out.println((double) 7 / 3); // Output: 2.3333333333333335
+ */
