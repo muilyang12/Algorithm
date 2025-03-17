c3e8e82918693ee3004901380f4bf3b349df2031
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2025-03-17 13:54:05 -0500

    LeetCode 62 Java.

---
+++ LeetCode - Algorithm/Dynamic Programming/62.java
@@ -0,0 +1,29 @@
+// 62. Unique Paths
+// https://leetcode.com/problems/unique-paths/
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] memo = new int[m][n];
+
+        for (int i = 0; i < m; i++)
+            memo[i][n - 1] = 1;
+        for (int j = 0; j < n; j++)
+            memo[m - 1][j] = 1;
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                memo[i][j] = memo[i + 1][j] + memo[i][j + 1];
+            }
+        }
+
+        return memo[0][0];
+    }
+}
+
+/*
+ * m = 3, n = 7
+ * 
+ * [0,0,0,0,0,0,1]
+ * [0,0,0,0,0,0,1]
+ * [1,1,1,1,1,1,1]
+ */
